3900080325911f7821d33cde75bc93f75b41a21b
Erik Humphrey <ContactErikH@gmail.com>
2017-12-14 18:38:52 -0500

    Update 5.2 Duplicating 2D List Values.py

--- 5 Linear Collections/5.2 Duplicating 2D List Values.py
+++ 5 Linear Collections/5.2 Duplicating 2D List Values.py
@@ -1,9 +1,9 @@
 lst = [[1, 2, 3], ["ha", "ke", "ja"], [3.141, 23.366, 1.0101]]
 
-def double(2d_list):
-	for i in range(len(2d_list)):
-		for j in range(len(2d_list)):
-			cool_list[i][j] *= 2
-	return cool_list
+def double(twodim):
+	for i in range(len(twodim)):
+		for j in range(len(twodim)):
+			twodim[i][j] *= 2
+	return twodim
 
 print(double(lst))
